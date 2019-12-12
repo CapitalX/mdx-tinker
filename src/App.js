a3cab12b41e15702c8090577c52196ca78df7453
Max <maxeust@gmail.com>
2019-12-12 10:40:08 +0100

    deploys

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 import {Editor, Viewer } from './Canvas.js';
 
-const deploy = file => {
-  //
-}
 
 function App() {
   const [code, setCode] = useState('');
+  const deploy = async file => {
+    const payload = JSON.stringify({
+      'main.mdx': file
+    });
+    const response = await fetch(process.env.REACT_APP_ENDPOINT, {
+      method: 'POST',
+      body: payload,
+    });
+    console.log(response);
+    //
+  }
   return (
     <div className="flex flex-column w-100 h-100">
       <div className="flex flex-row w-100 h-75">
